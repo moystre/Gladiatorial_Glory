387fc1d88e0c0b36c09386e4dae918548bfed023
moystre <moystre@gmail.com>
2018-02-28 14:35:44 +0100

    Knight prefab & Stand_animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float Speed = 1f;
+
+    private Vector2 movementDirection;
+    private Rigidbody2D rbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rbody.velocity = movementDirection * Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        dir = dir.normalized;
+        movementDirection = dir;
+    }
+}
+
+
