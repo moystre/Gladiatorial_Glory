eb40cca89d55b21ad84a773eecbc545bf0497a5a
moystre <moystre@gmail.com>
2018-03-06 13:19:23 +0100

    Knight animation
    walk & run

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,31 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public float WalkSpeed = 1f;
+    public float RunSpeed = 2f;
 
-    public float Speed = 1f;
-
+    private float speed = 0f;
     private Vector2 movementDirection;
     private Rigidbody2D rbody;
+    private Animator animator;
+    private SpriteRenderer _renderer;
+
+//    public bool isWalking = false;
+//    public bool isRunning = false;
 
     // Use this for initialization
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
+        speed = WalkSpeed;
     }
+
+
 
     private void FixedUpdate()
     {
-        rbody.velocity = movementDirection * Speed;
+        rbody.velocity = movementDirection;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        dir = dir.normalized;
-        movementDirection = dir;
+        if (Input.GetKeyDown(KeyCode.Q))
+            animator.SetTrigger("Die");
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+            movementDirection = Vector2.zero;
+        }
+        else
+            Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (x < 0f)
+            _renderer.flipX = true;
+        else if (x > 0f)
+            _renderer.flipX = false;
+
+        Vector2 dir = new Vector2(x, y);
+        speed = 0f;
+        if (dir.magnitude > 0f)
+        {
+            animator.SetBool("isWalking", true);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = RunSpeed;
+                animator.SetBool("isRunning", true);
+         
+            }
+            else
+            {
+              
+                animator.SetBool("isRunning", false);
+                speed = WalkSpeed;
+             
+            }
+        }else
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isWalking", false);
+        }
+        movementDirection = dir.normalized * speed;
     }
 }
-
-
